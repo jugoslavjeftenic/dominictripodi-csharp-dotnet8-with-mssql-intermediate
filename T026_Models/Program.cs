73acc1088f3b3e5295da73bb3859004d0197c09c
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-01-08 13:41:47 +0100

    T026_Models

---
+++ T026_Models/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace T026_Models
+{
+	public class Computer
+	{
+		public string Motherboard { get; set; } = "";
+		public int CPUCores { get; set; }
+		public string VideoCard { get; set; } = "";
+		public bool HasWifi { get; set; }
+		public bool HasLTE { get; set; }
+		public DateTime ReleaseDate { get; set; }
+		public decimal Price { get; set; }
+	}
+
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			Computer myComputer = new()
+			{
+				Motherboard = "Z690",
+				CPUCores = 8,
+				VideoCard = "RTX 2060",
+				HasWifi = true,
+				HasLTE = true,
+				ReleaseDate = DateTime.Now,
+				Price = 948.87m
+			};
+
+			myComputer.HasWifi = false;
+
+			Console.WriteLine(myComputer.Motherboard);
+			Console.WriteLine(myComputer.CPUCores);
+			Console.WriteLine(myComputer.VideoCard);
+			Console.WriteLine(myComputer.HasWifi);
+			Console.WriteLine(myComputer.HasLTE);
+			Console.WriteLine(myComputer.ReleaseDate);
+			Console.WriteLine(myComputer.Price);
+		}
+	}
+}
