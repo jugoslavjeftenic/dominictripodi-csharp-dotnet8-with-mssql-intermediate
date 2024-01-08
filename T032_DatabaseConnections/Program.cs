dbd1b32216d84160a1480ab8e8c1c47245445806
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-01-08 14:39:53 +0100

    T032_DatabaseConnections

---
+++ T032_DatabaseConnections/Program.cs
@@ -0,0 +1,13 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+string connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=DotNetCourseDatabase;TrustServerCertificate=true;Trusted_Connection=true;";
+
+IDbConnection dbConnection = new SqlConnection(connectionString);
+
+string sqlCommand = "SELECT GETDATE()";
+
+DateTime rightNow = dbConnection.QuerySingle<DateTime>(sqlCommand);
+
+Console.WriteLine(rightNow);
