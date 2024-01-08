6e7e2791c42b5748936e7c39483ed0f25ef5ce61
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-01-08 15:33:57 +0100

    T034_Dapper

---
+++ T034_Dapper/Models/ComputerModel.cs
@@ -0,0 +1,13 @@
+﻿namespace T034_Dapper.Models
+{
+	internal class ComputerModel
+	{
+		public string Motherboard { get; set; } = "";
+		public int CPUCores { get; set; }
+		public string VideoCard { get; set; } = "";
+		public bool HasWifi { get; set; }
+		public bool HasLTE { get; set; }
+		public DateTime ReleaseDate { get; set; }
+		public decimal Price { get; set; }
+	}
+}
---
+++ T034_Dapper/Program.cs
@@ -0,0 +1,71 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Globalization;
+using T034_Dapper.Models;
+
+string connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=DotNetCourseDatabase;TrustServerCertificate=true;Trusted_Connection=true;";
+
+IDbConnection dbConnection = new SqlConnection(connectionString);
+
+ComputerModel myComputer = new()
+{
+	Motherboard = "Z690",
+	CPUCores = 8,
+	VideoCard = "RTX 2060",
+	HasWifi = true,
+	HasLTE = true,
+	ReleaseDate = DateTime.Now,
+	Price = 948.87m
+};
+
+//myComputer.ReleaseDate.ToString("yyyy-MM-dd")
+//myComputer.Price.ToString("0.00", CultureInfo.InvariantCulture)
+
+string sqlInsert = @$"INSERT INTO TutorialAppSchema.Computer (
+	Motherboard,
+	CPUCores,
+	VideoCard,
+	HasWifi,
+	HasLTE,
+	ReleaseDate,
+	Price
+) VALUES (
+	'{myComputer.Motherboard}',
+	'{myComputer.CPUCores}',
+	'{myComputer.VideoCard}',
+	'{myComputer.HasWifi}',
+	'{myComputer.HasLTE}',
+	'{myComputer.ReleaseDate:yyyy-MM-dd}',
+	'{myComputer.Price.ToString("0.00", CultureInfo.InvariantCulture)}')";
+
+//dbConnection.Execute(sqlInsert);
+
+string sqlSelect = @"SELECT 
+	Computer.Motherboard,
+	Computer.CPUCores,
+	Computer.VideoCard,
+	Computer.HasWifi,
+	Computer.HasLTE,
+	Computer.ReleaseDate,
+	Computer.Price
+FROM TutorialAppSchema.Computer";
+
+IEnumerable<ComputerModel> computers = dbConnection.Query<ComputerModel>(sqlSelect);
+
+int recordNo = 1;
+foreach (var computer in computers)
+{
+	string row = $"{recordNo++}: ";
+	row += $"[{computer.Motherboard}], ";
+	row += $"[{computer.CPUCores}], ";
+	row += $"[{computer.VideoCard}], ";
+	row += $"[{computer.HasWifi}], ";
+	row += $"[{computer.HasLTE}], ";
+	row += $"[{computer.ReleaseDate}], ";
+	row += $"[{computer.Price}]";
+
+	Console.WriteLine(row);
+}
+
+
