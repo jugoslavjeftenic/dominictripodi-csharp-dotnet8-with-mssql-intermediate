3a7c479f5caea547c1dc4f5fd58f1ddaff797919
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-01-13 13:05:24 +0100

    T039_FileReadAndWrite

---
+++ T039_FileReadAndWrite/Models/ComputerModel.cs
@@ -0,0 +1,14 @@
+﻿namespace T039_FileReadAndWrite.Models
+{
+	internal class ComputerModel
+	{
+		public int ComputerId { get; set; }
+		public string Motherboard { get; set; } = "";
+		public int CPUCores { get; set; }
+		public string VideoCard { get; set; } = "";
+		public bool HasWifi { get; set; }
+		public bool HasLTE { get; set; }
+		public DateTime ReleaseDate { get; set; }
+		public decimal Price { get; set; }
+	}
+}
---
+++ T039_FileReadAndWrite/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+using T039_FileReadAndWrite.Models;
+
+ComputerModel myComputer = new()
+{
+	Motherboard = "Z690",
+	CPUCores = 8,
+	VideoCard = "RTX 2060",
+	HasWifi = true,
+	HasLTE = true,
+	ReleaseDate = DateTime.Now,
+	Price = 948.87m
+};
+
+string sqlInsert = @$"INSERT INTO TutorialAppSchema.Computer (
+	Motherboard,
+	CPUCores,
+	VideoCard,
+	HasWifi,
+	HasLTE,
+	ReleaseDate,
+	Price
+) VALUES (
+	'{myComputer.Motherboard}',
+	'{myComputer.CPUCores}',
+	'{myComputer.VideoCard}',
+	'{myComputer.HasWifi}',
+	'{myComputer.HasLTE}',
+	'{myComputer.ReleaseDate:yyyy-MM-dd}',
+	'{myComputer.Price.ToString("0.00", CultureInfo.InvariantCulture)}')";
+
+File.WriteAllText("log.txt", sqlInsert);
+
+using StreamWriter openFile = new("log.txt", append: true);
+openFile.WriteLine(sqlInsert);
+openFile.Close();
+
+Console.WriteLine(File.ReadAllText("log.txt"));
