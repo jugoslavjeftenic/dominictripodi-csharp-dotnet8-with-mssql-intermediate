cf5f6f2b633868ccacc581bc46318498b7c747f6
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-01-13 02:32:01 +0100

    T037_Config

---
+++ T037_Config/Data/DataContextEF.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using T037_Config.Models;
+
+namespace T037_Config.Data
+{
+	internal class DataContextEF(IConfiguration config) : DbContext
+	{
+		private readonly IConfiguration _config = config;
+
+		public DbSet<ComputerModel> Computer { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			if (!optionsBuilder.IsConfigured)
+			{
+				optionsBuilder.UseSqlServer(_config.GetConnectionString("DefaultConnection"),
+					optionsBuilder => optionsBuilder.EnableRetryOnFailure());
+			}
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			string schemaName = "TutorialAppSchema";
+			modelBuilder.HasDefaultSchema(schemaName);
+
+			modelBuilder.Entity<ComputerModel>();
+		}
+	}
+}
---
+++ T037_Config/Program.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Configuration;
+using T037_Config.Data;
+using T037_Config.Models;
+
+IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+DataContextDapper dapper = new(config);
+DataContextEF ef = new(config);
+
+// Dapper
+string sqlSelect = @"SELECT 
+	Computer.ComputerId,
+	Computer.Motherboard,
+	Computer.CPUCores,
+	Computer.VideoCard,
+	Computer.HasWifi,
+	Computer.HasLTE,
+	Computer.ReleaseDate,
+	Computer.Price
+FROM TutorialAppSchema.Computer";
+
+IEnumerable<ComputerModel> computersDapper = dapper.LoadData<ComputerModel>(sqlSelect);
+
+foreach (var computer in computersDapper)
+{
+	string row = $"{computer.ComputerId}: ";
+	row += $"[{computer.Motherboard}], ";
+	row += $"[{computer.CPUCores}], ";
+	row += $"[{computer.VideoCard}], ";
+	row += $"[{computer.HasWifi}], ";
+	row += $"[{computer.HasLTE}], ";
+	row += $"[{computer.ReleaseDate}], ";
+	row += $"[{computer.Price}]";
+
+	Console.WriteLine(row);
+}
+
+Console.WriteLine();
+Console.WriteLine();
+
+// EntityFramework
+IEnumerable<ComputerModel> computersEF = ef.Computer.ToList<ComputerModel>();
+
+foreach (var computer in computersEF)
+{
+	string row = $"{computer.ComputerId}: ";
+	row += $"[{computer.ComputerId}], ";
+	row += $"[{computer.Motherboard}], ";
+	row += $"[{computer.CPUCores}], ";
+	row += $"[{computer.VideoCard}], ";
+	row += $"[{computer.HasWifi}], ";
+	row += $"[{computer.HasLTE}], ";
+	row += $"[{computer.ReleaseDate}], ";
+	row += $"[{computer.Price}]";
+
+	Console.WriteLine(row);
+}
