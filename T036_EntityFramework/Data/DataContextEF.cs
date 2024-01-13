2389270834438f83f589d9abb1434d87c918df13
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-01-13 01:10:24 +0100

    T036_EntityFramework

---
+++ T036_EntityFramework/Data/DataContextEF.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using T036_EntityFramework.Models;
+
+namespace T036_EntityFramework.Data
+{
+	internal class DataContextEF : DbContext
+	{
+		public DbSet<ComputerModel> Computer { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			string connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=DotNetCourseDatabase;TrustServerCertificate=true;Trusted_Connection=true;";
+
+			if (!optionsBuilder.IsConfigured)
+			{
+				optionsBuilder.UseSqlServer(connectionString,
+					optionsBuilder => optionsBuilder.EnableRetryOnFailure());
+			}
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			string schemaName = "TutorialAppSchema";
+			modelBuilder.HasDefaultSchema(schemaName);
+
+			//string tableName = "Computer";
+			//modelBuilder.Entity<ComputerModel>().ToTable(tableName, schemaName);
+
+			modelBuilder.Entity<ComputerModel>();
+			//modelBuilder.Entity<ComputerModel>().HasNoKey();
+			//modelBuilder.Entity<ComputerModel>().HasKey(c => c.ComputerId);
+		}
+	}
+}
---
+++ T036_EntityFramework/Program.cs
@@ -0,0 +1,38 @@
+﻿using T036_EntityFramework.Data;
+using T036_EntityFramework.Models;
+
+DataContextEF entityFramework = new();
+
+ComputerModel myComputer = new()
+{
+	Motherboard = "B550",
+	CPUCores = 6,
+	VideoCard = "RX 470",
+	HasWifi = true,
+	HasLTE = true,
+	ReleaseDate = DateTime.Now,
+	Price = 948.87m
+};
+
+// Add to DB
+//entityFramework.Add(myComputer);
+//entityFramework.SaveChanges();
+
+// Read from DB
+IEnumerable<ComputerModel> computers = entityFramework.Computer.ToList<ComputerModel>();
+
+int recordNo = 1;
+foreach (var computer in computers)
+{
+	string row = $"{recordNo++}: ";
+	row += $"[{computer.ComputerId}], ";
+	row += $"[{computer.Motherboard}], ";
+	row += $"[{computer.CPUCores}], ";
+	row += $"[{computer.VideoCard}], ";
+	row += $"[{computer.HasWifi}], ";
+	row += $"[{computer.HasLTE}], ";
+	row += $"[{computer.ReleaseDate}], ";
+	row += $"[{computer.Price}]";
+
+	Console.WriteLine(row);
+}
